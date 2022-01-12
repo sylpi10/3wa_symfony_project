ba0af5b22fcc8fca293be8021afabbf658a738c4
sylpi <syl.pillet@hotmail.fr>
2022-01-12 15:34:28 +0100

    refactor organisateur template

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,14 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\Checkpoint;
 use App\Entity\FinalUser;
-use App\Entity\Producteur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
